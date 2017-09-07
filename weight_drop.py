6373b586c4bba071226a55c3ff923b6bef11371e
Stephen Merity <smerity@smerity.com>
2017-09-06 20:43:24 -0700

    Fix Weight Drop by monkey patching CUDNN RNN flatten parameters call

--- weight_drop.py
+++ weight_drop.py
@@ -11,14 +11,23 @@ class WeightDrop(torch.nn.Module):
         self.variational = variational
         self._setup()
 
+    def widget_demagnetizer_y2k_edition(*args, **kwargs):
+        # We need to replace flatten_parameters with a nothing function
+        # It must be a function rather than a lambda as otherwise pickling explodes
+        # We can't write boring code though, so ... WIDGET DEMAGNETIZER Y2K EDITION!
+        # (╯°□°）╯︵ ┻━┻
+        return
+
     def _setup(self):
+        # Terrible temporary solution to an issue regarding compacting weights re: CUDNN RNN
+        if issubclass(type(self.module), torch.nn.RNNBase):
+            self.module.flatten_parameters = self.widget_demagnetizer_y2k_edition
+
         for name_w in self.weights:
             print('Applying weight drop of {} to {}'.format(self.dropout, name_w))
             w = getattr(self.module, name_w)
-            wgts = self.module._all_weights[0]
             del self.module._parameters[name_w]
             self.module.register_parameter(name_w + '_raw', Parameter(w.data))
-            wgts[wgts.index(name_w)] = name_w + '_raw'
 
     def _setweights(self):
         for name_w in self.weights:
@@ -38,13 +47,53 @@ class WeightDrop(torch.nn.Module):
         return self.module.forward(*args)
 
 if __name__ == '__main__':
-    x = torch.nn.Linear(10, 10)
-    x.bias.data *= 0
-    y = torch.nn.functional.dropout(torch.autograd.Variable(torch.ones(10, 1)), p=0.5, training=True)
-    z = torch.autograd.Variable(torch.rand(2, 10))
-    print(x.weight)
-    print(y)
-    print(x(z))
-    x.weight.data = (y.expand_as(x.weight) * x.weight).data
-    print(x.weight)
-    print(x(z))
+    import torch
+    from weight_drop import WeightDrop
+
+    # Input is (seq, batch, input)
+    x = torch.autograd.Variable(torch.randn(2, 1, 10)).cuda()
+    h0 = None
+
+    ###
+
+    print('Testing WeightDrop')
+    print('=-=-=-=-=-=-=-=-=-=')
+
+    ###
+
+    print('Testing WeightDrop with Linear')
+
+    lin = WeightDrop(torch.nn.Linear(10, 10), ['weight'], dropout=0.9)
+    lin.cuda()
+    run1 = [x.sum() for x in lin(x).data]
+    run2 = [x.sum() for x in lin(x).data]
+
+    print('All items should be different')
+    print('Run 1:', run1)
+    print('Run 2:', run2)
+
+    assert run1[0] != run2[0]
+    assert run1[1] != run2[1]
+
+    print('---')
+
+    ###
+
+    print('Testing WeightDrop with LSTM')
+
+    wdrnn = WeightDrop(torch.nn.LSTM(10, 10), ['weight_hh_l0'], dropout=0.9)
+    wdrnn.cuda()
+
+    run1 = [x.sum() for x in wdrnn(x, h0)[0].data]
+    run2 = [x.sum() for x in wdrnn(x, h0)[0].data]
+
+    print('First timesteps should be equal, all others should differ')
+    print('Run 1:', run1)
+    print('Run 2:', run2)
+
+    # First time step, not influenced by hidden to hidden weights, should be equal
+    assert run1[0] == run2[0]
+    # Second step should not
+    assert run1[1] != run2[1]
+
+    print('---')

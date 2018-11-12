890b70caad25ba84143a00b6a5048a6373b0b155
Noémien Kocher <nkcr.je@gmail.com>
2018-11-12 15:27:33 +0100

    Uses "args.device" for device-agnostic code

--- generate.py
+++ generate.py
@@ -12,6 +12,8 @@ from torch.autograd import Variable
 
 import data
 
+from utils import init_device
+
 parser = argparse.ArgumentParser(description='PyTorch PTB Language Model')
 
 # Model parameters.
@@ -27,8 +29,8 @@ parser.add_argument('--words', type=int, default='1000',
                     help='number of words to generate')
 parser.add_argument('--seed', type=int, default=1111,
                     help='random seed')
-parser.add_argument('--cuda', action='store_true',
-                    help='use CUDA')
+parser.add_argument('--no-cuda', action='store_true',
+                    help='do not use CUDA')
 parser.add_argument('--temperature', type=float, default=1.0,
                     help='temperature - higher will increase diversity')
 parser.add_argument('--log-interval', type=int, default=100,
@@ -37,32 +39,23 @@ args = parser.parse_args()
 
 # Set the random seed manually for reproducibility.
 torch.manual_seed(args.seed)
-if torch.cuda.is_available():
-    if not args.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
-    else:
-        torch.cuda.manual_seed(args.seed)
+# Sets the `args.device` and CUDA seed.
+init_device(args)
 
 if args.temperature < 1e-3:
     parser.error("--temperature has to be greater or equal 1e-3")
 
 with open(args.checkpoint, 'rb') as f:
-    model = torch.load(f)
+    model = torch.load(f, map_location=args.device)
 model.eval()
 if args.model == 'QRNN':
     model.reset()
-
-if args.cuda:
-    model.cuda()
-else:
-    model.cpu()
 
 corpus = data.Corpus(args.data)
 ntokens = len(corpus.dictionary)
 hidden = model.init_hidden(1)
 input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
-if args.cuda:
-    input.data = input.data.cuda()
+input.data = input.data.to(args.device)
 
 with open(args.outf, 'w') as outf:
     for i in range(args.words):

7175311dd13d59f90c48752e790d33ee131b9c51
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2018-12-30 18:18:47 -0500

    Use pytoune main and model as the real main and model now

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import argparse
 import time
 import math
@@ -9,12 +10,25 @@ import torch
 import torch.nn as nn
 
 import data
-import model as m
+from data import SentenceLoader
+import pytoune_model as m
 
 from utils import batchify, get_batch, repackage_hidden
 from splitcross import SplitCrossEntropyLoss
 
+from pytoune.framework import Experiment as PytouneExperiment
+from pytoune.framework.callbacks import ClipNorm, ReduceLROnPlateau
+from callbacks import *
 
+
+def get_source_directory(directory_name):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), directory_name)
+
+
+def get_experiment_directory(directory_name):
+    default_dir = get_source_directory('./results')
+    dest_directory = os.environ.get('RESULTS_DIR', default_dir)
+    return os.path.join(dest_directory, directory_name)
 
 
 def main():
@@ -50,91 +64,6 @@ def main():
     args = parser.parse_args()
     args.tied = True
 
-    def model_save(fn):
-        with open(fn, 'wb') as f:
-            torch.save([model, criterion, optimizer], f)
-
-
-    def model_load(fn):
-        global model, criterion, optimizer
-        with open(fn, 'rb') as f:
-            model, criterion, optimizer = torch.load(f)
-
-
-    def train():
-        # Turn on training mode which enables dropout.
-        if args.model == 'QRNN': model.reset()
-        total_loss = 0
-        start_time = time.time()
-        ntokens = len(corpus.dictionary)
-        hidden = model.init_hidden(args.batch_size)
-        batch, i = 0, 0
-        while i < train_data.size(0) - 1 - 1:
-            bptt = args.bptt if np.random.random() < 0.95 else args.bptt / 2.
-            # Prevent excessively small or negative sequence lengths
-            seq_len = max(5, int(np.random.normal(bptt, 5)))
-            # There's a very small chance that it could select a very long sequence length resulting in OOM
-            # seq_len = min(seq_len, args.bptt + 10)
-
-            lr2 = optimizer.param_groups[0]['lr']
-            optimizer.param_groups[0]['lr'] = lr2 * seq_len / args.bptt
-            model.train()
-            data, targets = get_batch(train_data, i, args, seq_len=seq_len)
-
-            # Starting each batch, we detach the hidden state from how it was previously produced.
-            # If we didn't, the model would try backpropagating all the way to start of the dataset.
-            hidden = repackage_hidden(hidden)
-            optimizer.zero_grad()
-
-            output, hidden, rnn_hs, dropped_rnn_hs = model(data, hidden, return_h=True)
-            raw_loss = criterion(model.decoder.weight, model.decoder.bias, output, targets)
-
-            loss = raw_loss
-            # Activiation Regularization
-            if args.alpha: loss = loss + sum(args.alpha * dropped_rnn_h.pow(2).mean() for dropped_rnn_h in dropped_rnn_hs[-1:])
-            # Temporal Activation Regularization (slowness)
-            if args.beta: loss = loss + sum(args.beta * (rnn_h[1:] - rnn_h[:-1]).pow(2).mean() for rnn_h in rnn_hs[-1:])
-            loss.backward()
-
-            # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
-            if args.clip: torch.nn.utils.clip_grad_norm_(params, args.clip)
-            optimizer.step()
-
-            total_loss += raw_loss.data
-            optimizer.param_groups[0]['lr'] = lr2
-            if batch % args.log_interval == 0 and batch > 0:
-                cur_loss = total_loss.item() / args.log_interval
-                elapsed = time.time() - start_time
-                print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5f} | ms/batch {:5.2f} | '
-                        'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
-                    epoch, batch, len(train_data) // args.bptt, optimizer.param_groups[0]['lr'],
-                    elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss), cur_loss / math.log(2)))
-                total_loss = 0
-                start_time = time.time()
-            ###
-            batch += 1
-            i += seq_len
-
-    def evaluate(data_source, batch_size=10):
-        # Turn on evaluation mode which disables dropout.
-        model.eval()
-        if args.model == 'QRNN': model.reset()
-        total_loss = 0
-        ntokens = len(corpus.dictionary)
-        hidden = model.init_hidden(batch_size)
-        for i in range(0, data_source.size(0) - 1, args.bptt):
-            data, targets = get_batch(data_source, i, args.bptt)
-            import pdb; pdb.set_trace()
-            output, hidden = model(data, hidden)
-            loss = criterion(model.decoder.weight, model.decoder.bias, output, targets).data
-            total_loss += len(data) * loss
-            print(loss.data, file=open('main_valid.logs', 'a+'))
-            hidden = repackage_hidden(hidden)
-        return total_loss.item() / len(data_source)
-
-
-
-
     # Set the random seed manually for reproducibility.
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
@@ -162,10 +91,9 @@ def main():
     val_data = batchify(corpus.valid, eval_batch_size, args)
     test_data = batchify(corpus.test, test_batch_size, args)
 
-    ###############################################################################
-    # Build the model
-    ###############################################################################
-    criterion = None
+    train_loader = SentenceLoader(train_data, args.bptt)
+    valid_loader = SentenceLoader(val_data, args.bptt, False)
+    test_loader = SentenceLoader(test_data, args.bptt, False)
 
     ntokens = len(corpus.dictionary)
     model = m.RNNModel(
@@ -179,129 +107,68 @@ def main():
         args.dropouti,
         args.dropoute,
         args.wdrop,
-        args.tied
+        args.tied,
+        args.alpha,
+        args.beta,
+        args.batch_size
     )
 
-    ###
-    if args.resume:
-        print('Resuming model ...')
-        model_load(args.resume)
-        optimizer.param_groups[0]['lr'] = args.lr
-        model.dropouti, model.dropouth, model.dropout, args.dropoute = args.dropouti, args.dropouth, args.dropout, args.dropoute
-        if args.wdrop:
-            from weight_drop import WeightDrop
-            for rnn in model.rnns:
-                if type(rnn) == WeightDrop: rnn.dropout = args.wdrop
-                elif rnn.zoneout > 0: rnn.zoneout = args.wdrop
+    if args.model == 'QRNN': model.reset()
 
     ###
-    if not criterion:
-        splits = []
-        if ntokens > 500000:
-            # One Billion
-            # This produces fairly even matrix mults for the buckets:
-            # 0: 11723136, 1: 10854630, 2: 11270961, 3: 11219422
-            splits = [4200, 35000, 180000]
-        elif ntokens > 75000:
-            # WikiText-103
-            splits = [2800, 20000, 76000]
-        print('Using', splits)
-        criterion = SplitCrossEntropyLoss(args.emsize, splits=splits, verbose=False)
-    ###
-
-    if args.cuda:
-        model = model.cuda()
-        criterion = criterion.cuda()
-
-    ###
-    params = list(model.parameters()) + list(criterion.parameters())
+    params = list(model.parameters())
     total_params = sum(x.size()[0] * x.size()[1] if len(x.size()) > 1 else x.size()[0] for x in params if x.size())
     print('Args:', args)
     print('Model total parameters:', total_params)
 
-    ###############################################################################
-    # Training code
-    ###############################################################################
+    optimizer = None
+    if args.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(params, lr=args.lr, weight_decay=args.wdecay)
+    if args.optimizer == 'adam':
+        optimizer = torch.optim.Adam(params, lr=args.lr, weight_decay=args.wdecay)
 
+    device = None
+    device_id = 0
+    if torch.cuda.is_available():
+        torch.cuda.set_device(device_id) # Fix bug where memory is allocated on GPU0 when ask to take GPU1.
+        device = torch.device('cuda:%d' % device_id)
+        logging.info("Training on GPU %d" % device_id)
+    else:
+        logging.info("Training on CPU")
 
-    # Loop over epochs.
-    lr = args.lr
-    best_val_loss = []
-    stored_loss = 100000000
+    dataset = args.data.split('/')[-1]
+    model_name = "AWD_{}_{}".format(args.model, dataset)
+    expt_name = './expt_{}'.format(model_name)
+    expt_dir = get_experiment_directory(expt_name)
+    expt = PytouneExperiment(
+        expt_dir,
+        model,
+        device=device,
+        optimizer=optimizer,
+        monitor_metric='val_loss',
+        monitor_mode='min'
+    )
 
-    # At any point you can hit Ctrl + C to break out of training early.
+    callbacks = [
+        HiddenInitCallback(args.batch_size, eval_batch_size),
+        HiddenRepackagingCallback(),
+        ClipNorm(params, args.clip),
+        # EvaluationCallback(),
+        ASGDOptimizerSwitchCallback(args),
+        ReduceLROnPlateau(monitor='val_loss', mode='min', patience=20, factor=0.5, threshold_mode='abs', threshold=1e-3, verbose=True),
+        AdaptativeLRSchedulerCallback(train_loader),
+    ]
+
     try:
-        optimizer = None
-        # Ensure the optimizer is optimizing params, which includes both the model's weights as well as the criterion's weight (i.e. Adaptive Softmax)
-        if args.optimizer == 'sgd':
-            optimizer = torch.optim.SGD(params, lr=args.lr, weight_decay=args.wdecay)
-        if args.optimizer == 'adam':
-            optimizer = torch.optim.Adam(params, lr=args.lr, weight_decay=args.wdecay)
-        for epoch in range(1, args.epochs+1):
-            epoch_start_time = time.time()
-            train()
-            if 't0' in optimizer.param_groups[0]:
-                tmp = {}
-                for prm in model.parameters():
-                    tmp[prm] = prm.data.clone()
-                    prm.data = optimizer.state[prm]['ax'].clone()
-
-                val_loss2 = evaluate(val_data)
-                print('-' * 89)
-                print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                    'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
-                        epoch, (time.time() - epoch_start_time), val_loss2, math.exp(val_loss2), val_loss2 / math.log(2)))
-                print('-' * 89)
-
-                if val_loss2 < stored_loss:
-                    model_save(args.save)
-                    print('Saving Averaged!')
-                    stored_loss = val_loss2
-
-                for prm in model.parameters():
-                    prm.data = tmp[prm].clone()
-
-            else:
-                val_loss = evaluate(val_data, eval_batch_size)
-                print('-' * 89)
-                print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                    'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
-                epoch, (time.time() - epoch_start_time), val_loss, math.exp(val_loss), val_loss / math.log(2)))
-                print('-' * 89)
-
-                if val_loss < stored_loss:
-                    model_save(args.save)
-                    print('Saving model (new best validation)')
-                    stored_loss = val_loss
-
-                if args.optimizer == 'sgd' and 't0' not in optimizer.param_groups[0] and (len(best_val_loss)>args.nonmono and val_loss > min(best_val_loss[:-args.nonmono])):
-                    print('Switching to ASGD')
-                    optimizer = torch.optim.ASGD(model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
-
-                if epoch in args.when:
-                    print('Saving model before learning rate decreased')
-                    model_save('{}.e{}'.format(args.save, epoch))
-                    print('Dividing learning rate by 10')
-                    optimizer.param_groups[0]['lr'] /= 10.
-
-                best_val_loss.append(val_loss)
-
+        expt.train(train_loader, valid_loader, callbacks=callbacks, seed=args.seed)
     except KeyboardInterrupt:
         print('-' * 89)
         print('Exiting from training early')
 
-    # Load the best saved model.
-    model_load(args.save)
+    print("Testing on test set...")
+    expt.test(test_loader)
 
-    # Run on test data.
-    test_loss = evaluate(test_data, test_batch_size)
-    print('=' * 89)
-    print('| End of training | test loss {:5.2f} | test ppl {:8.2f} | test bpc {:8.3f}'.format(
-        test_loss,
-        math.exp(test_loss),
-        test_loss / math.log(2)
-    ))
-    print('=' * 89)
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
     main()
--- model.py
+++ model.py
@@ -1,7 +1,11 @@
 import logging
+import numpy as np
 import torch
 import torch.nn as nn
 
+from pytoune.framework.metrics import acc
+
+from splitcross import SplitCrossEntropyLoss
 from embed_regularize import embedded_dropout
 from locked_dropout import LockedDropout
 from weight_drop import WeightDrop
@@ -9,8 +13,9 @@ from weight_drop import WeightDrop
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, wdrop=0, tie_weights=False, alpha=2, beta=1):
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, wdrop=0, tie_weights=False, alpha=2, beta=1, bsz=20):
         super(RNNModel, self).__init__()
+        self.bsz = bsz
         self.ntoken = ntoken
         self.rnn_type = rnn_type
         self.ninp = ninp
@@ -23,6 +28,7 @@ class RNNModel(nn.Module):
         self.tie_weights = tie_weights
         self.alpha = alpha
         self.beta = beta
+        self.metrics = [self.acc, self.perplexity]
 
         self.lockdrop = LockedDropout()
         self.idrop = nn.Dropout(dropouti)
@@ -64,32 +70,47 @@ class RNNModel(nn.Module):
 
 
         # Build the SplitCrossEntropyLoss criterion here
-        # self.build_criterion()
+        self.build_criterion()
+
+        self.hidden = None
 
     def build_criterion(self):
         splits = []
-        if self.ntokens > 500000:
+        if self.ninp > 500000:
             # One Billion
             # This produces fairly even matrix mults for the buckets:
             # 0: 11723136, 1: 10854630, 2: 11270961, 3: 11219422
             splits = [4200, 35000, 180000]
-        elif self.ntokens > 75000:
+        elif self.ninp > 75000:
             # WikiText-103
             splits = [2800, 20000, 76000]
         logging.info('Using splits: {}'.format(' '.join(splits)))
         self.criterion = SplitCrossEntropyLoss(self.ninp, splits=splits, verbose=False)
 
     def loss_function(self, X_pred, X_true):
-        output, hidden, rnn_hs, dropped_rnn_hs = X_pred
-        loss = self.criterion(self.decoder.weight, self.decoder.bias, X_pred, X_true)
+        if type(X_pred) == tuple:
+            output, rnn_hs, dropped_rnn_hs = X_pred
+            loss = self.criterion(self.decoder.weight, self.decoder.bias, output, X_true)
 
-        # Activiation Regularization
-        if args.alpha: loss = loss + sum(args.alpha * dropped_rnn_h.pow(2).mean() for dropped_rnn_h in dropped_rnn_hs[-1:])
+            # Activiation Regularization
+            if self.alpha: loss = loss + sum(self.alpha * dropped_rnn_h.pow(2).mean() for dropped_rnn_h in dropped_rnn_hs[-1:])
 
-        # Temporal Activation Regularization (slowness)
-        if args.beta: loss = loss + sum(args.beta * (rnn_h[1:] - rnn_h[:-1]).pow(2).mean() for rnn_h in rnn_hs[-1:])
-
+            # Temporal Activation Regularization (slowness)
+            if self.beta: loss = loss + sum(self.beta * (rnn_h[1:] - rnn_h[:-1]).pow(2).mean() for rnn_h in rnn_hs[-1:])
+        else:
+            output = X_pred
+            loss = self.criterion(self.decoder.weight, self.decoder.bias, output, X_true)
         return loss
+
+    def acc(self, X_pred, X_true):
+        if type(X_pred) == tuple:
+            output, rnn_hs, dropped_rnn_hs = X_pred
+        else:
+            output = X_pred
+        return acc(self.decoder(output), X_true)
+
+    def perplexity(self, X_pred, X_true):
+        return np.exp(self.loss_function(X_pred, X_true))
 
     def reset(self):
         if self.rnn_type == 'QRNN': [r.reset() for r in self.rnns]
@@ -100,7 +121,9 @@ class RNNModel(nn.Module):
         self.decoder.bias.data.fill_(0)
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, input, hidden, return_h=False):
+    def forward(self, input):
+        if input.shape[1] != self.hidden[0][0].shape[1]:
+            self.init_hidden(input.shape[1])
         emb = embedded_dropout(self.encoder, input, dropout=self.dropoute if self.training else 0)
         #emb = self.idrop(emb)
 
@@ -113,29 +136,44 @@ class RNNModel(nn.Module):
         outputs = []
         for l, rnn in enumerate(self.rnns):
             current_input = raw_output
-            raw_output, new_h = rnn(raw_output, hidden[l])
+            raw_output, new_h = rnn(raw_output, self.hidden[l])
             new_hidden.append(new_h)
             raw_outputs.append(raw_output)
             if l != self.nlayers - 1:
                 #self.hdrop(raw_output)
                 raw_output = self.lockdrop(raw_output, self.dropouth)
                 outputs.append(raw_output)
-        hidden = new_hidden
+        self.hidden = new_hidden
 
         output = self.lockdrop(raw_output, self.dropout)
         outputs.append(output)
 
         result = output.view(output.size(0)*output.size(1), output.size(2))
-        if return_h:
-            return result, hidden, raw_outputs, outputs
-        return result, hidden
+        if self.training:
+            return result, raw_outputs, outputs
+        return result
 
     def init_hidden(self, bsz):
         weight = next(self.parameters()).data
         if self.rnn_type == 'LSTM':
-            return [(weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_(),
+            self.hidden = [(weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_(),
                     weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_())
                     for l in range(self.nlayers)]
         elif self.rnn_type == 'QRNN' or self.rnn_type == 'GRU':
-            return [weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_()
+            self.hidden = [weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_()
                     for l in range(self.nlayers)]
+
+    def __repackage_single_hidden(self, h):
+        """Wraps hidden states in new Tensors,
+        to detach them from their history."""
+        if isinstance(h, torch.Tensor):
+            return h.detach()
+        else:
+            return tuple(self.__repackage_single_hidden(v) for v in h)
+
+    def repackage_hidden(self):
+        self.hidden = self.__repackage_single_hidden(self.hidden)
+
+    def eval(self):
+        self.init_hidden(self.bsz)
+        return super(RNNModel, self).eval()
